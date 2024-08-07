d15eaaf5537fe0cccbf7d356c28ba8a2f3f8f72a
Anand Chitipothu <anandology@gmail.com>
2024-08-07 20:36:09 +0530

    Generated industry-metadata.jsonl

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import click
 from ocems_tracker import scraper
 from ocems_tracker.industry import Industry
 import pandas as pd
+import shutil
 
 @click.group()
 def app():
@@ -9,7 +10,6 @@ def app():
 
 @app.command()
 def download_industries():
-    print("download industries")
     api = scraper.API()
 
     industries = api.get_all_industries()
@@ -17,6 +17,14 @@ def download_industries():
     df = pd.DataFrame(data)
     df.to_csv("data/industries.csv", index=False)
 
+
+@app.command()
+def download_industry_metadata():
+    api = scraper.API()
+    # this saves the JSONL file in cache
+    api.get_all_industry_metadata()
+    shutil.copy("cache/industry-metadata/all.jsonl", "data/industry-metadata.jsonl")
+    print("generated data/industry-metadata.jsonl")
 
 if __name__ == "__main__":
     app()
--- ocems_tracker/scraper.py
+++ ocems_tracker/scraper.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 import requests
 from niftyhacks.cache import DiskCache
 from dataclasses import dataclass, field
+import datetime
+import pytz
 
 cache = DiskCache("cache/")
 
@@ -26,12 +28,12 @@ class API:
         url = "https://rtdms.cpcb.gov.in/api/getAllState"
         return self.session.get(url).json()
 
-    @cache.memoize("state-{state_id}-cities.json")
+    @cache.memoize("cities/{state_id}.json")
     def get_cities(self, state_id):
         url = f"https://rtdms.cpcb.gov.in/api/getAllCity/{state_id}"
         return self.session.get(url).json()
 
-    @cache.memoize("industries-{state_id}-{city}.json")
+    @cache.memoize("industries/{state_id}-{city}.json")
     def get_industries(self, state_id, city):
         url = f"https://rtdms.cpcb.gov.in/api/industryList/45/{state_id}/{city}"
         return self.session.get(url).json()
@@ -40,6 +42,10 @@ class API:
     def get_all_industries(self):
         for city in self.get_all_cities():
             yield from self.get_industries(city['id'], city['city'])
+
+    def get_industry_ids(self):
+        industries = self.get_all_industries()
+        return sorted(industry['id'] for industry in industries)
 
     def get_all_active_industry_ids(self) -> set[int]:
         """Returns a set of ids of all active industries.
@@ -60,4 +66,62 @@ class API:
         for state in self.get_states():
             for city in self.get_cities(state["id"]):
                 yield city
+
+    @cache.memoize("industry-metadata/{industry_id}.json")
+    def get_industry_metadata(self, industry_id):
+        print("get_industry_metadata", industry_id)
+        url = f"https://rtdms.cpcb.gov.in/api/industryMapDetailNEW/{industry_id}"
+        print("GET", url)
+        data = self.session.get(url).json()
+        if not data:
+            data = {
+                "industry": {
+                    "id": industry_id
+                }
+            }
+        # remove recentData
+        data.pop('recentData', None)
+        return self.strip_sensitive_data(data)
+
+    @cache.memoize("industry-metadata/all.jsonl")
+    def get_all_industry_metadata(self):
+        for id in self.get_industry_ids():
+            yield self.get_industry_metadata(id)
+
+    def strip_sensitive_data(self, data):
+        """Removes sensitive fields like email, phone number, password and tokens.
+        """
+        def is_sensitive_key(key):
+            key = key.lower()
+            sensitive_names = ['email', 'phone', 'contactno', 'password', 'token']
+            return any(name in key for name in sensitive_names)
+
+        if isinstance(data, list):
+            return [self.strip_sensitive_data(d) for d in data]
+        elif isinstance(data, dict):
+            return {k: self.strip_sensitive_data(v) for k, v in data.items() if not is_sensitive_key(k)}
+        else:
+            return data
+
+    @cache.memoize("device-data/{date}/{station_id:06d}-{device_id:06d}.json")
+    def _get_device_data(self, date, station_id, device_id):
+        """Downloads the data for last 48 hours. The date is used as the cacke key
+        to avoid repeated downloads for the same day.
+        """
+        url = f"https://rtdms.cpcb.gov.in/api/stations/{station_id}/devices/{device_id}/data"
+
+
+    def get_device_data(self, station_id, device_id):
+        date = self.today()
+        return self._get_device_data(date, station_id, device_id)
+
+
+    def today(self) -> str:
+        """Returns Today in timezone IST as a string.
+        """
+        t = datetime.datetime.utcnow()
+        tz = pytz.timezone("Asia/Kolkata")
+        return pytz.utc.localize(t, is_dst=None).astimezone(tz)
+
+
 
