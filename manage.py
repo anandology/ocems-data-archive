241b8e58fba4161626254c844bab08fcaf6b9710
Anand Chitipothu <anandology@gmail.com>
2024-08-10 12:08:26 +0530

    Added functionality to download historical data

--- manage.py
+++ manage.py
@@ -2,7 +2,34 @@ import click
 from ocems_tracker import scraper
 from ocems_tracker.industry import Industry
 import pandas as pd
+from pathlib import Path
 import shutil
+from niftyhacks.cache import setup_logger
+import logging
+import csv
+import gzip
+
+# Disable urllib3 warnings
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+logger = logging.getLogger()
+
+
+def setup_logger(verbose=False, format="short"):
+    """Setup logger to print the logs to stdout.
+    """
+    level = logging.DEBUG if verbose else logging.INFO
+
+    format = '[%(asctime)s] %(message)s'
+    datefmt = '%Y-%d-%m %H:%M:%S'
+
+    logging.basicConfig(
+        level=level,
+        format=format,
+        datefmt=datefmt)
+
 
 @click.group()
 def app():
@@ -26,5 +53,131 @@ def download_industry_metadata():
     shutil.copy("cache/industry-metadata/all.jsonl", "data/industry-metadata.jsonl")
     print("generated data/industry-metadata.jsonl")
 
+@app.command()
+def download_param_metadata():
+    api = scraper.API()
+    # this saves the JSONL file in cache
+    api.get_all_param_metadata()
+    shutil.copy("cache/param-metadata.jsonl", "data/param-metadata.jsonl")
+    print("generated data/param-metadata.jsonl")
+
+@app.command
+def industry_status():
+    """Downloads status of all industries.
+    """
+    logger.info("Downloading industry status of all industries")
+    api = scraper.API()
+    data = api.get_all_industry_status()
+    date = api.today()
+    df = pd.DataFrame(data)
+    path = Path(f"daily/status/{date}.csv")
+    path.parent.mkdir(exist_ok=True, parents=True)
+    df.to_csv(path, index=False)
+    logger.info("saved the industry status to %s", path)
+
+@app.command()
+def live_data():
+    """Fetch live parameter values for all industries for yesterday.
+    """
+    api = scraper.API()
+    live = scraper.LiveDataScrapper(api)
+
+    data = live.get_all_live_data()
+    df = pd.DataFrame(data)
+    df.to_csv("live-data.csv", index=False)
+
+def split_data_by_year(path):
+    """Splits a csv.gz data file for an industry by year.
+    """
+    path = Path(path)
+    print("splitting", path)
+
+    root = Path(__file__).parent.resolve() / "archive"
+
+    files = {}
+    def get_file(year):
+        year = str(year)
+        if year not in files:
+            new_path = root / year / (path.name + ".tmp")
+            new_path.parent.mkdir(parents=True, exist_ok=True)
+            files[year] = gzip.open(new_path, "wt")
+            files[year].write(header)
+        return files[year]
+
+    with gzip.open(path, "rt") as f:
+        header = f.readline()
+
+        for line in f:
+            t = line.split(",")[-2] # eg: 2016-01-01 01:30:00:000
+            year = t.split("-")[0]
+            get_file(year).write(line)
+
+    for f in files.values():
+        f.close()
+        f2 = Path(f.name).with_suffix("")
+        shutil.move(f.name, f2.name)
+        print("saved", f2.name)
+
+@app.command
+def archive():
+    """Archive the historical data to internet archive item ocems-data-archive.
+    """
+    for p in sorted(Path("cache/history/").glob("*.csv.gz"), key=lambda p: int(p.name.split(".")[0])):
+        split_data_by_year(p)
+
+@app.command
+def historical_data():
+    api = scraper.API()
+    live = scraper.LiveDataScrapper(api)
+    industry_ids = [int(line) for line in open("active.txt")]
+
+    logger.info("Starting download of historical data")
+    logger.info("Found %d industries", len(industry_ids))
+
+    for industry_id in industry_ids:
+        download_historical_data(live, industry_id)
+        split_data_by_year(f"cache/history/{industry_id}.csv.gz")
+
+    logger.info("Download of historical data is complete")
+
+def download_historical_data(live, industry_id):
+    path = Path(f"cache/history/{industry_id}.csv.gz")
+    path.parent.mkdir(exist_ok=True, parents=True)
+    if path.exists():
+        logger.info("Data already downloaded for industry %s", industry_id)
+        return
+
+    logger.info("Downloading historical data for industry %s", industry_id)
+
+    path2 = path.with_suffix(".gz.tmp")
+
+    try:
+        data = live.get_historical_data(industry_id)
+        columns = "industry_id station_id device_id param_key param_label time value".split()
+        rows = ({k: str(v) for k, v in d.items()} for d in data)
+
+        with gzip.open(path2, "wt") as f:
+            w = csv.DictWriter(f, columns)
+            w.writeheader()
+            w.writerows(rows)
+
+        # df = pd.DataFrame(data)
+        # df.to_csv(path, index=False)
+        shutil.move(path2, path)
+        logger.info("saved %s", path)
+    finally:
+        if path2.exists():
+            logger.info("Deleting partial file %s", path2)
+            path2.unlink()
+
+@app.command()
+def test():
+    import json
+    api = scraper.API()
+    live = scraper.LiveDataScrapper(api)
+    data = live.get_param_values(1035, 31346, 47940, "pm")
+    list(data)
+
 if __name__ == "__main__":
+    setup_logger()
     app()
--- ocems_tracker/scraper.py
+++ ocems_tracker/scraper.py
@@ -9,8 +9,13 @@ from niftyhacks.cache import DiskCache
 from dataclasses import dataclass, field
 import datetime
 import pytz
+import logging
+import json
+import time
 
 cache = DiskCache("cache/")
+
+logger = logging.getLogger(__name__)
 
 headers = {
     "Referer": "https://rtdms.cpcb.gov.in/data/",
@@ -43,20 +48,31 @@ class API:
         for city in self.get_all_cities():
             yield from self.get_industries(city['id'], city['city'])
 
+    @cache.memoize("industry-ids.jsonl")
     def get_industry_ids(self):
         industries = self.get_all_industries()
         return sorted(industry['id'] for industry in industries)
 
-    def get_all_active_industry_ids(self) -> set[int]:
-        """Returns a set of ids of all active industries.
-        """
-        active = set()
-        for city in self.get_all_cities():
-            active |= {d['id'] for d in self._get_active_industries(city['id'], city['city'])}
-        return active
+    def get_all_industry_status(self):
+        industry_ids = self.get_industry_ids()
+        d = {row['id']: row['status'] for row in self._get_all_industry_live_status()}
 
-    @cache.memoize("industries-active/{state_id}-{city}.json")
-    def _get_active_industries(self, state_id, city):
+        for industry_id in industry_ids:
+            status = d.get(industry_id) or 'offline'
+            yield dict(industry_id=industry_id, status=status)
+
+    def _get_all_industry_live_status(self):
+        """Returns status of all live industries.
+
+        Returns a generator with one record for every live industry. Each record looks like the following:
+
+            {"id": 2364, "status": "live"}
+        """
+        for city in self.get_all_cities():
+            yield from self._get_industry_live_status(city['id'], city['city'])
+
+    def _get_industry_live_status(self, state_id, city):
+        logger.info("fetching live status for city %s - %s", state_id, city)
         url = f"https://rtdms.cpcb.gov.in/api/industryListStatus/45/{state_id}/{city}"
         r = self.session.get(url)
         return r.json()
@@ -103,25 +119,171 @@ class API:
         else:
             return data
 
-    @cache.memoize("device-data/{date}/{station_id:06d}-{device_id:06d}.json")
-    def _get_device_data(self, date, station_id, device_id):
-        """Downloads the data for last 48 hours. The date is used as the cacke key
-        to avoid repeated downloads for the same day.
-        """
-        url = f"https://rtdms.cpcb.gov.in/api/stations/{station_id}/devices/{device_id}/data"
 
-
-    def get_device_data(self, station_id, device_id):
-        date = self.today()
-        return self._get_device_data(date, station_id, device_id)
-
-
-    def today(self) -> str:
+    def today(self) -> datetime.date:
         """Returns Today in timezone IST as a string.
         """
         t = datetime.datetime.utcnow()
         tz = pytz.timezone("Asia/Kolkata")
-        return pytz.utc.localize(t, is_dst=None).astimezone(tz)
+        return pytz.utc.localize(t, is_dst=None).astimezone(tz).date()
+
+    def yesterday(self) -> datetime.date:
+        """Returns Today in timezone IST as a string.
+        """
+        date = self.today() - datetime.timedelta(days=1)
+        return date.date()
+
+    def get_all_industry_metadata_summary(self):
+        metadata = self.get_all_industry_metadata()
+
+    # @cache.memoize("tmp/status/{industry_id}.json")
+    # def get_industry_status(self, industry_id) -> str:
+    #     """Returns the industry status for an industry.
+
+    #     The possible response are:
+    #         - live
+    #         - offline
+    #         - error (server error in accessing the API)
+    #         - NA (No data available, API response is empty)
+    #     """
+    #     logger.info("get_industry_status %s", industry_id)
+
+    #     url = f"https://rtdms.cpcb.gov.in/api/industryMapDetailNEW/{industry_id}"
+    #     r = self.session.get(url)
+    #     if r.status_code != 200:
+    #         logger.error("response: %s %s", r.status_code, r.reason)
+    #         return 'error'
+    #     try:
+    #         d = r.json()
+    #     except Exception:
+    #         logger.error("Failed to parse json", exc_info=True)
+    #         return 'error'
+    #     return d.get('industryStatus') or 'error'
+
+    # def get_all_industry_status(self):
+    #     return [{"industry_id": id, "status": self.get_industry_status(id)} for id in self.get_industry_ids()]
+
+    @cache.memoize("param-metadata.jsonl")
+    def get_all_param_metadata(self):
+        for industry_id in self.get_industry_ids():
+            data = self.get_param_metadata(industry_id)
+            if data:
+                yield data
+
+    def get_param_metadata(self, industry_id):
+        def process_station(station_data):
+            station = {}
+            station['id'] = station_data['id']
+            station['name'] = station_data['name']
+            station['devices'] = [process_device(station['id'], d) for d in station_data['devices']]
+            return station
+
+        def process_device(station_id, device_data):
+            device = {}
+            device['id'] = device_data['id']
+            device['name'] = device_data['name']
+            device['params'] = [process_param(station_id, d) for d in device_data['params']]
+            return device
+
+        def process_param(station_id, param_data):
+            param = {}
+            d = param_data['stdParam']
+            param['id'] = d['id']
+            param['type'] = d['type']
+            param['name'] = d['name']
+            param['key'] = d['paramKey']
+            param['label'] = d['label']
+            param['unit'] = d['stdUnit']
+            try:
+                param['max'] = data['thresholdNEW'][str(station_id)][str(param['id'])]['max']
+            except KeyError:
+                pass
+            return param
+
+        def process_industry(data):
+            industry = {}
+            industry['id'] = data['industry']['id']
+            industry['name'] = data['industry']['name']
+            industry['stations'] = [process_station(d) for d in data['stations']]
+            return industry
+
+        data = self.get_industry_metadata(industry_id)
+        # No data
+        if "stations" not in data:
+            return None
+        return process_industry(data)
 
 
+class LiveDataScrapper:
+    """Utility to download live data for all industries.
+    """
+    def __init__(self, api):
+        self.api = api
+        self.session = api.session
+        self.param_metadata = api.get_all_param_metadata()
+        self.metadata_lookup = {d['id']: d for d in self.param_metadata}
+        # fetch last 2 days of data
+        self.start_date = "2d-ago"
 
+    def get_historical_data(self, industry_id):
+        self.start_date = "10y-ago"
+        date = "history"
+        return self._get_live_data(date, industry_id)
+
+    def get_live_data(self, industry_id):
+        date = self.api.today()
+        return self._get_live_data(date, industry_id)
+
+    def get_all_live_data(self):
+        for industry_id in self.api.get_industry_ids():
+            yield from self.get_live_data(industry_id)
+
+    # @cache.memoize("live-data/{date}/{industry_id}.jsonl")
+    def _get_live_data(self, date, industry_id):
+        if industry_id not in self.metadata_lookup:
+            return []
+        industry = self.metadata_lookup[industry_id]
+        for station in industry['stations']:
+            for device in station['devices']:
+                for param in device['params']:
+                    try:
+                        data = self.get_param_values(industry_id, station['id'], device['id'], param['key'])
+
+                        common = dict(industry_id=industry['id'],
+                                    station_id=station['id'],
+                                    device_id=device['id'],
+                                    param_key=param['key'],
+                                    param_label=param['label'])
+                        data2 = [dict(common, **d) for d in data[param['name']]]
+                        yield from data2
+                    except Exception:
+                        args = dict(industry_id=industry['id'],
+                                    station_id=station['id'],
+                                    device_id=device['id'],
+                                    param_key=param['key'])
+                        logger.error("FAILED PARAMS %s", json.dumps(args))
+                        logger.error("Failed to fetch param values", exc_info=True)
+
+    def get_param_values(self, industry_id, station_id, device_id, param_key):
+        logger.info("get_param_values %s %s %s %s", industry_id, station_id, device_id, param_key)
+        retries = 3
+        r = None
+        for i in range(retries):
+            try:
+                url = f"https://rtdms.cpcb.gov.in/api/stations/{station_id}/devices/{device_id}/data"
+                payload = {
+                    "avg": "minute_15",
+                    "param": param_key,
+                    "startDate": self.start_date
+                }
+                r = self.session.post(url, json=payload)
+                return r.json()
+            except Exception as e:
+                if i+1 < retries:
+                    logger.error("Failed with error %s", e)
+                    logger.error("Response: %s", r.text if r else "")
+                    logger.error("Retrying...")
+                    time.sleep(2)
+                else:
+                    logger.error("Failed even after retries")
+                    raise
